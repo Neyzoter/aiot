ab5d39fc4d84d97e729641588ac4fe2550dd15e7
Neyzoter <sonechaochao@gmail.com>
2020-02-10 20:53:58 +0800

    add fddp module

---
+++ fddp/src/main/java/cn/neyzoter/aiot/fddp/FddpApplication.java
@@ -0,0 +1,20 @@
+package cn.neyzoter.aiot.fddp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * fault detection and diagnosis platform
+ * @author Neyzoter Song
+ * @date 2020-2-10
+ */
+@SpringBootApplication
+public class FddpApplication {
+    private final static Logger logger = LoggerFactory.getLogger(FddpApplication.class);
+    public static void main(String[] args){
+        logger.info("Fddp Application Start...");
+        SpringApplication.run(FddpApplication.class, args);
+    }
+}
