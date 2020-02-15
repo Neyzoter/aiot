3bba04444e6a4793b9545823d7a87b098c841ad2
Neyzoter <sonechaochao@gmail.com>
2020-02-15 22:57:12 +0800

    update spark

--- iov/src/main/java/cn/neyzoter/aiot/iov/web/controller/VehicleController.java
+++ iov/src/main/java/cn/neyzoter/aiot/iov/web/controller/VehicleController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.TreeMap;
 
 
 /**
