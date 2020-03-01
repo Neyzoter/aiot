591fc45a2167821c606ff66016b97cc4948e7e8f
Neyzoter <sonechaochao@gmail.com>
2020-03-01 20:16:27 +0800

    make shell simple

--- shell/startup-all.sh
+++ shell/startup-all.sh
@@ -1,31 +1,10 @@
 #!/bin/bash
 
-
-# quick stark a kafka server
-# please check bootstrap-server IP and port parammeters in application.properties
-echo "KAFKA_HOME = $KAFKA_HOME"
-echo "Starting zookeeper..."
-nohup $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
-echo "Started zookeeper"
-
-echo "Starting kafka server..."
-nohup $KAFKA_HOME/bin/kafka-server-start.sh  $KAFKA_HOME/config/server.properties &
-echo "Started kafka server  OK"
-
-echo "Starting kafka server-2..."
-nohup $KAFKA_HOME/bin/kafka-server-start.sh  $KAFKA_HOME/config/server-2.properties &
-echo "Started kafka server-2 OK"
-
-echo "Starting kafka server-3..."
-nohup $KAFKA_HOME/bin/kafka-server-start.sh  $KAFKA_HOME/config/server-3.properties &
-echo "Started kafka server-3 OK"
-
-echo "Topics : "
-$KAFKA_HOME/bin/kafka-topics.sh --list --zookeeper localhost:2181
-echo "Started kafka server-3 OK"
-
+# quick kafka
+./kafka/kafka-quickstart.sh
 
 # quick stark InfluxDB
-echo "Starting InfluxDB ..."
-nohup influxd &
-echo "Started InfluxDB OK"
+./influxdb/influxdb-quickstart.sh
+
+# Quick start Spark
+./spark/spark-quickstart.sh
