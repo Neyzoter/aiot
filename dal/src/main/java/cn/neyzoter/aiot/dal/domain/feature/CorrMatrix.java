222ed0e566f0da9d340cfe6a3a1b0516666d6f78
Neyzoter <sonechaochao@gmail.com>
2020-04-29 10:35:29 +0800

    update

--- dal/src/main/java/cn/neyzoter/aiot/dal/domain/feature/CorrMatrix.java
+++ dal/src/main/java/cn/neyzoter/aiot/dal/domain/feature/CorrMatrix.java
@@ -9,5 +9,8 @@ import java.io.Serializable;
  */
 public class CorrMatrix implements Serializable {
     private static final long serialVersionUID = -4793223507478566000L;
-
+    double[][] matrix;
+    CorrMatrix (int size) {
+        this.matrix = new double[size][size];
+    }
 }
