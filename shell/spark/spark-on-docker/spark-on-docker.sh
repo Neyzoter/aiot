86ea82d159ec236bd8cad22e92685cfeb20e7fd9
Neyzoter <sonechaochao@gmail.com>
2020-07-05 19:33:34 +0800

    update xml

--- shell/spark/spark-on-docker/spark-on-docker.sh
+++ shell/spark/spark-on-docker/spark-on-docker.sh
@@ -1,3 +1,4 @@
+# 参考：https://blog.csdn.net/boling_cavalry/article/details/86851069
 wget https://raw.githubusercontent.com/zq2599/blog_demos/master/sparkdockercomposefiles/docker-compose.yml \
 && wget https://raw.githubusercontent.com/zq2599/blog_demos/master/sparkdockercomposefiles/hadoop.env \
 && docker-compose up -d
